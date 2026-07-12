23ea1470ab2e0ac7f84accae0dd2c98da77e6e36
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:49 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "data_factory_dataset_sql_server_tables" {
-  description = "All data_factory_dataset_sql_server_table resources"
-  value       = azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables
-}
 output "data_factory_dataset_sql_server_tables_additional_properties" {
-  description = "List of additional_properties values across all data_factory_dataset_sql_server_tables"
-  value       = [for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : v.additional_properties]
+  description = "Map of additional_properties values across all data_factory_dataset_sql_server_tables, keyed the same as var.data_factory_dataset_sql_server_tables"
+  value       = { for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : k => v.additional_properties }
 }
 output "data_factory_dataset_sql_server_tables_annotations" {
-  description = "List of annotations values across all data_factory_dataset_sql_server_tables"
-  value       = [for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : v.annotations]
+  description = "Map of annotations values across all data_factory_dataset_sql_server_tables, keyed the same as var.data_factory_dataset_sql_server_tables"
+  value       = { for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : k => v.annotations }
 }
 output "data_factory_dataset_sql_server_tables_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_dataset_sql_server_tables"
-  value       = [for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_dataset_sql_server_tables, keyed the same as var.data_factory_dataset_sql_server_tables"
+  value       = { for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : k => v.data_factory_id }
 }
 output "data_factory_dataset_sql_server_tables_description" {
-  description = "List of description values across all data_factory_dataset_sql_server_tables"
-  value       = [for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : v.description]
+  description = "Map of description values across all data_factory_dataset_sql_server_tables, keyed the same as var.data_factory_dataset_sql_server_tables"
+  value       = { for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : k => v.description }
 }
 output "data_factory_dataset_sql_server_tables_folder" {
-  description = "List of folder values across all data_factory_dataset_sql_server_tables"
-  value       = [for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : v.folder]
+  description = "Map of folder values across all data_factory_dataset_sql_server_tables, keyed the same as var.data_factory_dataset_sql_server_tables"
+  value       = { for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : k => v.folder }
 }
 output "data_factory_dataset_sql_server_tables_linked_service_name" {
-  description = "List of linked_service_name values across all data_factory_dataset_sql_server_tables"
-  value       = [for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : v.linked_service_name]
+  description = "Map of linked_service_name values across all data_factory_dataset_sql_server_tables, keyed the same as var.data_factory_dataset_sql_server_tables"
+  value       = { for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : k => v.linked_service_name }
 }
 output "data_factory_dataset_sql_server_tables_name" {
-  description = "List of name values across all data_factory_dataset_sql_server_tables"
-  value       = [for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : v.name]
+  description = "Map of name values across all data_factory_dataset_sql_server_tables, keyed the same as var.data_factory_dataset_sql_server_tables"
+  value       = { for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : k => v.name }
 }
 output "data_factory_dataset_sql_server_tables_parameters" {
-  description = "List of parameters values across all data_factory_dataset_sql_server_tables"
-  value       = [for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : v.parameters]
+  description = "Map of parameters values across all data_factory_dataset_sql_server_tables, keyed the same as var.data_factory_dataset_sql_server_tables"
+  value       = { for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : k => v.parameters }
 }
 output "data_factory_dataset_sql_server_tables_schema_column" {
-  description = "List of schema_column values across all data_factory_dataset_sql_server_tables"
-  value       = [for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : v.schema_column]
+  description = "Map of schema_column values across all data_factory_dataset_sql_server_tables, keyed the same as var.data_factory_dataset_sql_server_tables"
+  value       = { for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : k => v.schema_column }
 }
 output "data_factory_dataset_sql_server_tables_table_name" {
-  description = "List of table_name values across all data_factory_dataset_sql_server_tables"
-  value       = [for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : v.table_name]
+  description = "Map of table_name values across all data_factory_dataset_sql_server_tables, keyed the same as var.data_factory_dataset_sql_server_tables"
+  value       = { for k, v in azurerm_data_factory_dataset_sql_server_table.data_factory_dataset_sql_server_tables : k => v.table_name }
 }
 
